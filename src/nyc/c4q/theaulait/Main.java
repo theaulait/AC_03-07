f806ef555bab04c634d79ddd57c493d2b19d4fbd
theaulait <vanice.yee@gmail.com>
2015-03-07 13:05:02 -0500

    8th attempt

--- src/nyc/c4q/theaulait/Main.java
+++ src/nyc/c4q/theaulait/Main.java
@@ -1,8 +1,25 @@
 package nyc.c4q.theaulait;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.print("Quel age avez vous?");
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        int ticketPrice;
+
+        if (age <= 13){
+            ticketPrice = 8;
+        }
+         else if (age <= 64){
+            ticketPrice = 13;
+        }
+        else {
+            ticketPrice = 10;
+        }
+            System.out.println("Vous payez " + ticketPrice + " Euros");
+
     }
+
 }
